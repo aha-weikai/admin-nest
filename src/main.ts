e629aeac099692fe813c795cf751b44b15f9bba0
kaixin <1900461953@qq.com>
2023-08-14 17:31:07 +0800

    chore: 增加全局自定义请求参数校验

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+import { CustomValidationPipe } from './pipe/custom-validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 设置请求地址的前缀
+  // localhost:3000/api/
+  app.setGlobalPrefix('api');
+
+  // 全局使用管道验证(请求参数验证)
+  app.useGlobalPipes(new CustomValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/pipe/custom-validation.pipe.ts
@@ -0,0 +1,52 @@
+import { Injectable, ValidationError, ValidationPipe } from '@nestjs/common';
+
+@Injectable()
+export class CustomValidationPipe extends ValidationPipe {
+  // 必须重写此方法才能，实现自定义验证
+  protected flattenValidationErrors(
+    validationErrors: ValidationError[],
+  ): any[] {
+    console.log(validationErrors);
+    return validationErrors.map((err) => ({
+      field: err.property,
+      // message: err.constraints[Object.keys(err.constraints)[0]],
+      message: Object.values(err.constraints)[0],
+    }));
+  }
+}
+
+/*
+ validationErrors
+ [
+   ValidationError {
+     target: RegisterDto { account: '1', password: '111' },
+     value: '1',
+     property: 'account',
+     children: [],
+     constraints: {
+       isNotEmpty: '用户名不能为空',
+       isLength: 'account must be longer than or equal to 3 characters'
+     }
+   },
+   ValidationError {
+     target: RegisterDto { account: '1', password: '111' },
+     value: '111',
+     property: 'password',
+     children: [],
+     constraints: {
+       isLength: 'password must be longer than or equal to 6 characters'
+     }
+  }
+ ]
+ 从上面格式转为下面的格式
+ [
+   {
+     field: 'account',
+     message: '账号不能为空',
+   },
+   {
+     field: 'password',
+     message: '密码不能为空',
+   },
+ ];
+ */
