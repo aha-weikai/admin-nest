ce7ae67cd92bebecd1b3b5b20e7bcfabbfe5608c
aha-kai <weikai999828@gmail.com>
2023-08-22 17:18:29 +0800

    feat: 获取用户信息修改

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Get(':id')
-  getUserInfo(@Param() id) {
-    console.log(`output-id`, id);
-    console.log(typeof id.id);
-    return id;
+  @UseGuards(AuthGuard('jwt'))
+  @Get('user_info')
+  getUserInfo(@Request() req) {
+    return req.user;
   }
 
-  @Post('/update')
+  @Post('update')
   updateUserInfo(data) {
     console.log(data);
   }
