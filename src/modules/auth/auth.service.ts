5815f3f44a9db8606510bd560f3a56b62a8ef412
aha-kai <weikai999828@gmail.com>
2023-08-24 17:39:17 +0800

    style: 代码格式化

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import { PrismaService } from '@/common/prisma.service';
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { LoginDto } from './dto/login.dto';
-import { RegisterDto } from './dto/register.dto';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
