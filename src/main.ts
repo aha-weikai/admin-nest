76405c57e2a22e9a37fa9ba7cbe9a41a85093900
weikai <weikai999828@gmail.com>
2023-08-27 23:12:44 +0800

    chore: 增加全局拦截器

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { ValidationPipe } from '@nestjs/common';
 import { CustomValidationPipe } from './pipes/custom-validation.pipe';
+import { HttpStatus } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,12 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   // 全局使用管道验证(请求参数验证)
-  app.useGlobalPipes(new CustomValidationPipe());
+  app.useGlobalPipes(
+    new CustomValidationPipe({
+      whitelist: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
