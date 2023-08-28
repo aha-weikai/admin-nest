1509499fd77275b21c4d17ed17be2bb025d21985
weikai <weikai999828@gmail.com>
2023-08-29 00:06:12 +0800

    chore: 添加captcha模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './modules/user/user.module';
+import { CaptchaModule } from './modules/captcha/captcha.module';
 
 @Module({
-  imports: [CommonModule, AuthModule, UserModule],
+  imports: [CommonModule, AuthModule, UserModule, CaptchaModule],
 })
 export class AppModule {}
